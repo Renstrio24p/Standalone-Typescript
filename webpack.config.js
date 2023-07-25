ab23c8a784bf3378419138713f2d4fa079eb422d
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-07-25 09:54:41 +0800

    update files

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
