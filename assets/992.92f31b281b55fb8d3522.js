3fb97c178762ec106fe98db31e6bf7c00866e0b8
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-10-04 20:38:43 +0000

    deploy: 949cd5fd2ea69978f14101ed7422ed12085bed88

---
+++ assets/992.92f31b281b55fb8d3522.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkstandalone_typescript=self.webpackChunkstandalone_typescript||[]).push([[992],{992:(e,t,n)=>{function s(e){let t=0;const n=n=>{t=n,e.innerHTML=`count is ${t}`};e.addEventListener("click",(()=>n(t+1))),n(0)}n.r(t),n.d(t,{setupCounter:()=>s})}}]);
