ebb996340d2a36779be067da5c8c39540f88b57b
Waren Gador <waren.gador15@gmail.com>
2023-10-22 10:03:06 +0800

    add files

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,17 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/[name].[contenthash].js',
-  },
-  target: 'web',
-  devServer: {
-    port: 4500,
+module.exports = (env:any, argv:any) => {
+  const isProduction = argv.mode === 'production';
+  const startTime = Date.now();
+
+  const devServerOptions = {
+    port: '3600',
     proxy: {
       '/api': {
         target: 'http://localhost:8800',
@@ -22,120 +19,152 @@ module.exports = {
     },
     static: {
       directory: path.join(__dirname, 'src'),
+      // publicPath: '/'
     },
     open: true,
-    hot: true,
-    liveReload: true,
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.json', '.scss'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x)?$/,
-        use: {
-          loader: 'esbuild-loader',
-          options: {
-            loader: 'tsx',
-            target: 'es6',
-          },
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.module\.(sa|sc|c)ss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
+    hot: !isProduction,
+    liveReload: !isProduction,
+    historyApiFallback: true,
+  };
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'assets/[name].[contenthash].js',
+      chunkFilename: 'assets/[name].[contenthash].js',
+      // publicPath: '/',
+    },
+    target: 'web',
+    devServer: devServerOptions,
+    resolve: {
+      extensions: ['.mjs', '.js','.ts', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /node_modules/,
+          use: {
+            loader: 'esbuild-loader',
             options: {
-              modules: true,
+              target: 'es2015',
             },
           },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /\.module\.(sa|sc|c)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name].[contenthash][ext]',
         },
-      },
-      {
-        test: /\.(mp4|webm|ogg|ogv)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'videos/[name].[contenthash][ext]',
-        },
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        terserOptions: {
-          ecma: 2018,
-          compress: {
-            comparisons: false,
-            inline: 2,
-          },
-          output: {
-            comments: false,
-            ascii_only: true,
+        {
+          test: /\.ts?$/,
+          use: {
+            loader: 'esbuild-loader',
+            options: {
+              target: 'es2015',
+              minify: true,
+            },
           },
         },
-        extractComments: false,
+        {
+          test: /\.(c|sa|sc)ss$/,
+          exclude: /\.module\.(c|sa|sc)ss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+            {
+              loader: 'esbuild-loader',
+              options: {
+                loader: 'css',
+                minify: true,
+                target: 'es2015',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.module\.(c|sa|sc)ss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+            'sass-loader',
+            {
+              loader: 'esbuild-loader',
+              options: {
+                loader: 'css',
+                minify: true,
+                target: 'es2015',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name].[contenthash][ext]',
+          },
+        },
+        {
+          test: /\.(mp4|webm|ogg|ogv)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'videos/[name].[contenthash][ext]',
+          },
+        },
+        {
+          test: /node_modules\/svelte\/.*\.mjs$/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, './', 'index.html'),
       }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/images',
+            to: 'images',
+          },
+          {
+            from: 'src/videos',
+            to: 'videos',
+          },
+        ],
+      }),
+      new Dotenv(),
     ],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 2000,
-      maxSize: 20000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: '~',
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-          name: 'vendors',
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
+    optimization: {
+      minimize: isProduction,
+      splitChunks: {
+        chunks: 'async',
+        minSize: 244 * 1024, // 244 KiB
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
         },
       },
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'index.html'),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/images',
-          to: 'images',
-        },
-        {
-          from: 'src/videos',
-          to: 'videos',
-        },
-      ],
-    }),
-    new Dotenv(),
-  ],
+    performance: {
+      maxAssetSize: 244000,
+      maxEntrypointSize: 244000,
+    },
+    cache: {
+      type: 'filesystem',
+    },
+    stats: 'errors-warnings',
+  };
 };
